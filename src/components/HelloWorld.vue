66dd7422503ecfae07e3213ce86458102cdd1da5
MarkoTON <markotoncanovic@gmail.com>
2021-08-03 11:27:49 +0200

    first commit - please check build > utils.js file

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <ul>
+      <li>Some rendom text</li>
+      <li>Some rendom text</li>
+      <li>Some rendom text</li>
+      <li>Some rendom text</li>
+    </ul>
+    <hr>
+    <button class="btn">Test button for SCSS</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+ul {
+  background: $kkk;
+}
+</style>
